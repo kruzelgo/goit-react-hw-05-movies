28d4e163c65146f9172e0b18ef0b5d5ca09c1797
kruzelgo <kruzelgo@gmail.com>
2024-05-11 19:46:38 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,12 @@ export const App = () => {
   return (
     <div
       style={{
-        height: '100vh',
+        // height: '100vh',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
       React homework template
